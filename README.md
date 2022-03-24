43f079e1871536de981abdf4e84f4f4c640da777
Abnumality <83428762+Abnumality@users.noreply.github.com>
2022-03-24 19:22:18 +0000

    add stuff

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 <h2>This is a frontend made for the Ultraviolet proxy. (BETA)</h2>
 
 # <a href='https://github.com/titaniumnetwork-dev/ultraviolet-node'>View Source</a>
--- public/index.js
+++ public/index.js
@@ -27,3 +27,41 @@ function loadMario() {
 function loadFireboyWatergirl() {
     window.location.replace('g4mescreen/fbwg.html');
 }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+
+showTime();
